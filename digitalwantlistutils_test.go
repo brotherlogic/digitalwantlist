c191bf1cd3e093e980d06d9c70ba6577fe6354c0
Simon Tucker <brotherlogic@gmail.com>
2020-08-22 17:24:45 -0700

    Adds in processing.

--- digitalwantlistutils_test.go
+++ digitalwantlistutils_test.go
@@ -3,12 +3,37 @@ package main
 import (
 	"context"
 	"testing"
+
+	"github.com/brotherlogic/keystore/client"
+
+	pb "github.com/brotherlogic/digitalwantlist/proto"
 )
 
 func InitTest() *Server {
 	s := Init()
 	s.SkipLog = true
+
+	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
+	s.GoServer.KSclient.Save(context.Background(), CONFIG, &pb.Config{})
+
 	return s
+}
+
+func TestLoad(t *testing.T) {
+	s := InitTest()
+	_, err := s.loadConfig(context.Background())
+	if err != nil {
+		t.Errorf("Bad config load: %v", err)
+	}
+}
+
+func TestBadLoad(t *testing.T) {
+	s := InitTest()
+	s.GoServer.KSclient.Fail = true
+	conf, err := s.loadConfig(context.Background())
+	if err == nil {
+		t.Errorf("Bad config load: %v", conf)
+	}
 }
 
 func TestBasic(t *testing.T) {
