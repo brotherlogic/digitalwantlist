3c8f42582cbea27429e99b8d3a0c8e8d2079e29b
Simon Tucker <brotherlogic@gmail.com>
2020-09-21 07:29:23 -0700

    Better

---
+++ cli/cli.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/brotherlogic/goserver/utils"
+
+	pbrc "github.com/brotherlogic/recordcollection/proto"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
+	defer cancel()
+
+	conn, err := utils.LFDialServer(ctx, "digitalwantlist")
+	if err != nil {
+		log.Fatalf("Pah: %v", err)
+	}
+	defer conn.Close()
+
+	switch os.Args[1] {
+	case "sforce":
+		val, _ := strconv.Atoi(os.Args[2])
+		client := pbrc.NewClientUpdateServiceClient(conn)
+		resp, err := client.ClientUpdate(ctx, &pbrc.ClientUpdateRequest{InstanceId: int32(val)})
+
+		fmt.Printf("%v and %v\n", resp, err)
+	}
+}
