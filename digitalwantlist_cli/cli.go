1c5f52a526feeae36260fed7adf61cb6b71226b8
Simon Tucker <brotherlogic@gmail.com>
2022-01-12 18:55:27 -0800

    Adds fullping method

--- digitalwantlist_cli/cli.go
+++ digitalwantlist_cli/cli.go
@@ -23,6 +23,27 @@ func main() {
 	defer conn.Close()
 
 	switch os.Args[1] {
+	case "fullping":
+		client := pbrc.NewClientUpdateServiceClient(conn)
+		ctx2, cancel2 := utils.ManualContext("recordcollectioncli-"+os.Args[1], time.Hour)
+		defer cancel2()
+
+		conn2, err := utils.LFDialServer(ctx2, "recordcollection")
+		if err != nil {
+			log.Fatalf("Cannot reach rc: %v", err)
+		}
+		defer conn2.Close()
+
+		registry := pbrc.NewRecordCollectionServiceClient(conn2)
+		ids, err := registry.QueryRecords(ctx2, &pbrc.QueryRecordsRequest{Query: &pbrc.QueryRecordsRequest_FolderId{673768}})
+		if err != nil {
+			log.Fatalf("Bad query: %v", err)
+		}
+		for i, id := range ids.GetInstanceIds() {
+			_, err := client.ClientUpdate(ctx, &pbrc.ClientUpdateRequest{InstanceId: id})
+
+			fmt.Printf("%v and %v\n", i, err)
+		}
 	case "sforce":
 		val, _ := strconv.Atoi(os.Args[2])
 		client := pbrc.NewClientUpdateServiceClient(conn)
